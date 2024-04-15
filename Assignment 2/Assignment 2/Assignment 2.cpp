32c38c481541dadaa0b7665028ef034504f2b8ae
MoodyJaZZzz <danciubov1@gmail.com>
2024-04-15 21:36:11 +0100

    2nd commit

--- Assignment 2/Assignment 2/Assignment 2.cpp
+++ Assignment 2/Assignment 2/Assignment 2.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <string>
- 
+#include <cstdlib> 
+#include <ctime>   
 
 using namespace std;
 
 
-int partition(string arr[], int low, int high) 
-{
-    string pivot = arr[high]; // choose the last element as the pivot
+int partition(string arr[], int low, int high) {
+    string pivot = arr[high]; // pivot=last element
     int i = low - 1;
 
-    for (int j = low; j < high; ++j) 
-    {
-        if (arr[j].compare(pivot) < 0) 
-        { 
+    for (int j = low; j < high; ++j) {
+        if (arr[j].compare(pivot) < 0) { 
             ++i;
             swap(arr[i], arr[j]);
         }
@@ -23,29 +21,26 @@ int partition(string arr[], int low, int high)
 }
 
 
-void quickSort(string arr[], int low, int high) 
-{
-    if (low < high) 
-    {
+void quickSort(string arr[], int low, int high) {
+    if (low < high) {
         int pi = partition(arr, low, high);
 
-        // Recursively sort elements before partition and after partition
+        // sort elements using recursion
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
 
-int binarySearch(string arr[], int low, int high, string key) 
-{
-    while (low <= high) 
-    {
+int binarySearch(string arr[], int low, int high, string key) {
+    while (low <= high) {
         int mid = low + (high - low) / 2;
 
         
         if (arr[mid] == key)
             return mid;
 
+        
         if (arr[mid] < key)
             low = mid + 1;
         
@@ -83,7 +78,7 @@ int main() {
                 cin >> strings[i];
             }
 
-            
+            //sort the array
             quickSort(strings, 0, SIZE - 1);
 
             
@@ -92,24 +87,57 @@ int main() {
                 cout << "String " << i + 1 << ": " << strings[i] << endl;
             }
 
-            // Search in the array
+            // Search the array
             string key;
             cout << "\nEnter a string to search: ";
             cin >> key;
             int index = binarySearch(strings, 0, SIZE - 1, key);
-            if (index != -1) 
-            {
+            if (index != -1) {
                 cout << "String found at index: " << index << endl;
             }
-            else 
-            {
+            else {
                 cout << "String not found." << endl;
             }
             break;
         }
-        case 2:
-            cout << "Guess The Number" << endl;
+        case 2: {
+            srand(time(0)); // Seed the random number generator with the current time
+
+            
+            int randomNumber = rand() % 1000 + 1;
+
+            int guess;
+            int attempts = 0;
+            const int maxAttempts = 10;
+
+            cout << "Welcome to the Guess the Number game!" << endl;
+            cout << "I have selected a number between 1 and 1000. You have " << maxAttempts << " attempts to guess it." << endl;
+
+            
+            while (attempts < maxAttempts) {
+                cout << "Attempt " << attempts + 1 << ": Enter your guess: ";
+                cin >> guess;
+
+                if (guess == randomNumber) {
+                    cout << "Congratulations! You guessed the number " << randomNumber << " correctly!" << endl;
+                    break;
+                }
+                else if (guess < randomNumber) {
+                    cout << "Too low. Try again." << endl;
+                }
+                else {
+                    cout << "Too high. Try again." << endl;
+                }
+
+                attempts++;
+            }
+
+            
+            if (attempts == maxAttempts) {
+                cout << "Sorry, you have used all your attempts. The number was " << randomNumber << "." << endl;
+            }
             break;
+        }
         case 3:
             cout << "EXIT" << endl;
             quit = true;
